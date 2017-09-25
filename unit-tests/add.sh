832ae3be0dcd4d6c1581a2aa1186d491767529c5
Simon Brooke <simon@journeyman.cc>
2017-09-25 10:01:25 +0100

    New unit tests.

---
+++ unit-tests/add.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+expected='5'
+actual=`echo "(add 2 3)" | target/psse 2> /dev/null | head -1`
+
+if [ "${expected}" = "${actual}" ]
+then
+    echo "OK"
+    exit 0
+else
+    echo "Fail: expected '${expected}', got '${actual}'"
+    exit 1
+fi
+
+expected='5.5000'
+actual=`echo "(add 2.5 3)" | target/psse 2> /dev/null | head -1`
+
+if [ "${expected}" = "${actual}" ]
+then
+    echo "OK"
+    exit 0
+else
+    echo "Fail: expected '${expected}', got '${actual}'"
+    exit 1
+fi
+
---
+++ unit-tests/apply.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+expected='1'
+actual=`echo "(apply 'add '(1))"| target/psse 2> /dev/null | head -1`
+
+if [ "${expected}" = "${actual}" ]
+then
+    echo "OK"
+    exit 0
+else
+    echo "Fail: expected '${expected}', got '${actual}'"
+    exit 1
+fi
