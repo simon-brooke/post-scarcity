98120d045b2cee70c3fb8f0427abe719801974f2
Simon Brooke <simon@journeyman.cc>
2018-12-23 14:16:51 +0000

    All unit tests now pass

---
+++ unit-tests/nlambda.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+expected='a'
+actual=`echo "((nlambda (x) x) a)" | target/psse 2> /dev/null | head -2 | tail -1`
+
+if [ "${expected}" = "${actual}" ]
+then
+    echo "OK"
+    exit 0
+else
+    echo "Fail: expected '${expected}', got '${actual}'"
+    exit 1
+fi
